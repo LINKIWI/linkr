8ab3753265b5532099f6746db6b5f41922b74d1c
Kevin Lin <developer@kevinlin.info>
2016-12-02 10:28:44 -0600

    More link and main URIs

--- uri/link.py
+++ uri/link.py
@@ -3,4 +3,19 @@ from base_uri import URI
 
 class LinkAddURI(URI):
     methods = ['PUT']
-    path = '/api/link/add'
+    path = '/linkr/api/link/add'
+
+
+class LinkDeleteURI(URI):
+    methods = ['DELETE']
+    path = '/linkr/api/link/delete'
+
+
+class LinkAliasRedirectURI(URI):
+    methods = ['GET', 'POST']
+    path = '/<alias>'
+
+
+class LinkNotFoundURI(URI):
+    methods = ['GET']
+    path = '/linkr/not-found'
--- uri/main.py
+++ uri/main.py
@@ -2,8 +2,20 @@ from base_uri import URI
 
 
 class HomeURI(URI):
+    methods = ['GET']
     path = '/'
 
 
-class AliasRedirectURI(URI):
-    path = '/<alias>'
+class DefaultURI(URI):
+    methods = ['GET']
+    path = '/<path:path>'
+
+
+class AdminURI(URI):
+    methods = ['GET']
+    path = '/linkr/admin'
+
+
+class APIDocumentationURI(URI):
+    methods = ['GET']
+    path = '/linkr/api'
