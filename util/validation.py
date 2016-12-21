df45ad1fadf2e67135e6582c5ec7e3033e3b996d
Kevin Lin <developer@kevinlin.info>
2016-12-21 11:39:15 -0800

    Significantly simpler validation scheme

--- util/validation.py
+++ util/validation.py
@@ -1,75 +1,37 @@
 import re
+import urlparse
+
 import requests
-
-
-# Shoutout to the poor soul who sat down to write this 500+ character regular expression for matching valid URLs.
-# Source: https://gist.github.com/dperini/729294
-VALID_URL_REGEX = re.compile(
-    u"^"
-    # protocol identifier
-    u"(?:(?:https?|ftp)://)"
-    # user:pass authentication
-    u"(?:\S+(?::\S*)?@)?"
-    u"(?:"
-    # IP address exclusion
-    # private & local networks
-    u"(?!(?:10|127)(?:\.\d{1,3}){3})"
-    u"(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})"
-    u"(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})"
-    # IP address dotted notation octets
-    # excludes loopback network 0.0.0.0
-    # excludes reserved space >= 224.0.0.0
-    # excludes network & broadcast addresses
-    # (first & last IP address of each class)
-    u"(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"
-    u"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}"
-    u"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
-    u"|"
-    # host name
-    u"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
-    # domain name
-    u"(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
-    # TLD identifier
-    u"(?:\.(?:[a-z\u00a1-\uffff]{2,}))"
-    u")"
-    # port number
-    u"(?::\d{2,5})?"
-    # resource path
-    u"(?:/\S*)?"
-    u"$",
-    re.UNICODE,
-)
-
-VALID_USERNAME_REGEX = re.compile('^[a-zA-Z0-9]+$')
 
 
 def is_alias_valid(alias):
     """
-    TODO
+    Check if an alias is valid. An alias is considered valid if it contains only URL-safe
+    characters and is shorter than a maximum length of 32 characters.
 
-    :param alias:
-    :return:
+    :param alias: The alias to validate.
+    :return: True if the alias is valid; False otherwise.
     """
-    # An alias is considered valid if it contains only URL-safe characters and shorter than a
-    # maximum length of 32 characters.
     return alias == requests.utils.quote(alias, safe='') and len(alias) < 32
 
 
 def is_url_valid(url):
     """
-    TODO
+    Check if a URL is valid. The URL is considered valid if a hostname can be parsed out of the
+    input string.
 
-    :param url:
-    :return:
+    :param url: The input URL to validate.
+    :return: True if the URL is valid; False otherwise.
     """
-    return bool(re.match(VALID_URL_REGEX, url))
+    return bool(urlparse.urlparse(url).netloc)
 
 
 def is_username_valid(username):
     """
-    TODO
+    Check if a username is valid.
 
-    :param username:
-    :return:
+    :param username: The username to validate.
+    :return: True if the username is valid; False otherwise.
     """
-    return bool(re.match(VALID_USERNAME_REGEX, username))
+    valid_username_regex = re.compile('^[a-zA-Z0-9]+$')
+    return bool(re.match(valid_username_regex, username))
