bd309ae9ad4527ee6914a69479260661758ee2d0
Kevin Lin <developer@kevinlin.info>
2016-12-21 19:20:19 -0800

    Remove Link is_active property

--- models/link.py
+++ models/link.py
@@ -9,7 +9,6 @@ class Link(db.Model):
 
     link_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     submit_time = db.Column(db.Integer)
-    is_active = db.Column(db.Boolean, default=True)
     hits = db.Column(db.Integer, default=0)
     password_hash = db.Column(db.Text, default=None)
     alias = db.Column(db.String(32), index=True, unique=True)
@@ -26,9 +25,6 @@ class Link(db.Model):
         self.outgoing_url = outgoing_url
         self.password_hash = util.cryptography.secure_hash(password) if password else None
 
-    def deactivate(self):
-        self.is_active = False
-
     def increment_hits(self):
         self.hits += 1
 
@@ -40,7 +36,6 @@ class Link(db.Model):
         return {
             'link_id': self.link_id,
             'submit_time': self.submit_time,
-            'is_active': self.is_active,
             'hits': self.hits,
             'alias': self.alias,
             'outgoing_url': self.outgoing_url,
