b80a0e7b084022ad4e3cd635fa938bdf6dceeb5c
Kevin Lin <developer@kevinlin.info>
2016-12-24 21:03:29 -0800

    Documentation on models.Link and add is_password_protected method

--- models/link.py
+++ models/link.py
@@ -6,6 +6,10 @@ from linkr import db
 
 
 class Link(db.Model):
+    """
+    Model representing a link entry.
+    """
+
     __tablename__ = 'link'
 
     link_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -22,6 +26,14 @@ class Link(db.Model):
         password=None,
         user_id=None,
     ):
+        """
+        Create a new link entry.
+
+        :param alias:
+        :param outgoing_url:
+        :param password:
+        :param user_id:
+        """
         self.submit_time = int(time.time())
         self.alias = alias
         self.outgoing_url = outgoing_url
@@ -29,10 +41,23 @@ class Link(db.Model):
         self.user_id = user_id
 
     def validate_password(self, password):
+        """
+        Validate that the supplied link password is correct for this link. This method will
+        consider any password to be "correct" if there is no password associated with this link.
+
+        :param password: The supplied password.
+        :return: True if the supplied password matches the link's password or if the link is not
+                 password protected; False otherwise.
+        """
         return not self.password_hash or \
             util.cryptography.secure_hash(password) == self.password_hash
 
     def as_dict(self):
+        """
+        Represent this link entry as a API-friendly, JSON-formatted dictionary.
+
+        :return: A representation of this link's data as a dictionary.
+        """
         return {
             'link_id': self.link_id,
             'user_id': self.user_id,
@@ -40,5 +65,13 @@ class Link(db.Model):
             'alias': self.alias,
             'full_alias': '{base}/{alias}'.format(base=config.options.LINKR_URL, alias=self.alias),
             'outgoing_url': self.outgoing_url,
-            'is_password_protected': bool(self.password_hash),
+            'is_password_protected': self.is_password_protected(),
         }
+
+    def is_password_protected(self):
+        """
+        Return whether this link is password protected.
+
+        :return: True if the link is password protected; False otherwise.
+        """
+        return bool(self.password_hash)
