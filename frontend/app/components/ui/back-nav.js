8708272a0846c04d42837097f01e3f6642f4a196
Kevin Lin <developer@kevinlin.info>
2016-12-25 17:22:16 -0800

    BackNav stateless component

---
+++ frontend/app/components/ui/back-nav.js
@@ -0,0 +1,21 @@
+import {browserHistory} from 'react-router';
+import KeyboardBackspace from 'react-icons/lib/md/keyboard-backspace';
+import React from 'react';
+
+/**
+ * Back arrow for reverse history navigation. Effectively, provides a UI component for programmatic
+ * invocation of the browser's back button.
+ */
+const BackNav = () => (
+  <div
+    className="back-nav margin--bottom text-orange transition"
+    onClick={browserHistory.goBack}
+  >
+    <KeyboardBackspace className="margin--right" />
+    <span className="sans-serif bold kilo">
+      GO BACK
+    </span>
+  </div>
+);
+
+export default BackNav;
