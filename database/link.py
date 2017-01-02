cb3c2d190a8337ba9f71004c3055f693401a222f
Kevin Lin <developer@kevinlin.info>
2017-01-01 18:03:37 -0800

    Link edit and password update database methods

--- database/link.py
+++ database/link.py
@@ -38,6 +38,48 @@ def add_link(alias, outgoing_url, password=None, user_id=None):
 
 
 @db_txn
+def edit_link(link_id, alias=None, outgoing_url=None):
+    """
+    Edit an existing link's details.
+
+    :param link_id: The ID of the link to edit.
+    :param alias: The new alias of the link, or None to leave it unchanged.
+    :param outgoing_url: The new outgoing URL of the link, or None to leave it unchanged.
+    :return: The models.Link instance representing the modified link object.
+    """
+    if alias and not util.validation.is_alias_valid(alias):
+        raise InvalidAliasException('Alias `{alias}` is not URL safe'.format(alias=alias))
+    if outgoing_url and not util.validation.is_url_valid(outgoing_url):
+        raise InvalidURLException('URL `{url}` is not a valid URL'.format(url=outgoing_url))
+
+    to_modify = get_link_by_id(link_id)
+    to_modify.edit(alias=alias, outgoing_url=outgoing_url)
+
+    db.session.add(to_modify)
+
+    return to_modify
+
+
+@db_txn
+def update_link_password(link_id, password):
+    """
+    Update a link's password. This method allows both adding a password to a previously
+    non-password-protected link, changing the password on a password-protected link, and removing
+    the password from a password-protected link.
+
+    :param link_id: ID of the link for which the password should be updated.
+    :param password: The new link password.
+    :return: The models.Link instance representing the modified Link object.
+    """
+    to_modify = get_link_by_id(link_id)
+    to_modify.update_password(password)
+
+    db.session.add(to_modify)
+
+    return to_modify
+
+
+@db_txn
 def delete_link(alias):
     """
     Delete a link from the database, if it exists.
