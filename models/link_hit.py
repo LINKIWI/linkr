7598c2a08f05a6f543e08d5d40b17b692df81e76
Kevin Lin <developer@kevinlin.info>
2017-01-05 12:27:42 -0800

    models.LinkHit documentation

--- models/link_hit.py
+++ models/link_hit.py
@@ -4,6 +4,10 @@ from linkr import db
 
 
 class LinkHit(db.Model):
+    """
+    Model representing an access to a link.
+    """
+
     __tablename__ = 'link_hit'
 
     hit_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -20,6 +24,14 @@ class LinkHit(db.Model):
         referer,
         user_agent,
     ):
+        """
+        Create a new link hit.
+
+        :param link_id: The ID of the link.
+        :param remote_ip: The IP address of the client.
+        :param referer: The referer header string, if applicable.
+        :param user_agent: The user-agent header string, if applicable.
+        """
         self.link_id = link_id
         self.timestamp = int(time.time())
         self.remote_ip = remote_ip
@@ -27,6 +39,11 @@ class LinkHit(db.Model):
         self.user_agent = user_agent
 
     def as_dict(self):
+        """
+        Represent this link hit as a API-friendly, JSON-formatted dictionary.
+
+        :return: A representation of this link hit's data as a dictionary.
+        """
         return {
             'hit_id': self.hit_id,
             'link_id': self.link_id,
