de095852e5bf19aa29014dd0a118540d4e97489c
Kevin Lin <developer@kevinlin.info>
2017-01-06 15:28:39 -0600

    Add util.browser.push

--- frontend/app/util/browser.js
+++ frontend/app/util/browser.js
@@ -1,13 +1,36 @@
 /* global window, setTimeout */
 
+import {browserHistory} from 'react-router';
+
+/**
+ * Load the specified URL after a delay.
+ *
+ * @param {String} url The URL to load.
+ * @param {Number=} delay Milliseconds to delay the page load.
+ */
 function go(url, delay) {
-  if (delay <= 0) {
+  if (!delay || delay <= 0) {
     window.location.href = url;
   } else {
     setTimeout(() => go(url, 0), delay);
   }
 }
 
+/**
+ * Push the specified path to browser history, handled via react-router.
+ *
+ * @param {String} path The path to push.
+ * @param {Number=} delay Milliseconds to delay the push.
+ */
+function push(path, delay) {
+  if (!delay || delay <= 0) {
+    browserHistory.push(path);
+  } else {
+    setTimeout(() => push(path, 0), delay);
+  }
+}
+
 export default {
-  go
+  go,
+  push
 };
