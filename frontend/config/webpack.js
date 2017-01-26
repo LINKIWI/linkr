64b796c1c42b76a66f9a301923a6485669227799
Kevin Lin <developer@kevinlin.info>
2017-01-26 09:42:20 -0600

    Remove comments in uglification

--- frontend/config/webpack.js
+++ frontend/config/webpack.js
@@ -3,7 +3,10 @@ var webpack = require('webpack');
 
 var plugins = [];
 if (process.env.NODE_ENV === 'production') {
-  plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      comments: false
+  }));
   plugins.push(new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
