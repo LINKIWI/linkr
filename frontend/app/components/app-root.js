4910998e5b05ecc98b0576d9d019901bbf5696ef
Kevin Lin <developer@kevinlin.info>
2017-02-09 20:31:04 -0600

    Favicon

--- frontend/app/components/app-root.js
+++ frontend/app/components/app-root.js
@@ -1,7 +1,12 @@
+import Favicon from 'react-favicon';
 import React from 'react';
 
 const AppRoot = ({children}) => (
   <div className="app-root">
+    <Favicon
+      animated={false}
+      url={['/static/img/favicon.png']}
+    />
     {children}
   </div>
 );
