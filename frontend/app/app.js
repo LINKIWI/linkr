2c7a688ce5f688ac604799e330bb8f4925261b38
Kevin Lin <developer@kevinlin.info>
2017-06-10 22:44:13 -0700

    Move global styles injection from AppRoot to App

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -2,6 +2,7 @@
 
 import bash from 'highlight.js/lib/languages/bash';
 import browserHistory from 'react-router/lib/browserHistory';
+import csjs from 'csjs-inject';
 import dottie from 'dottie';
 import js from 'highlight.js/lib/languages/javascript';
 import PiwikReactRouter from 'piwik-react-router';
@@ -10,7 +11,42 @@ import React from 'react';
 import Router from 'react-router/lib/Router';
 import {registerLanguage} from 'react-syntax-highlighter/dist/light';
 
+import fonts from '../resources/blobs/fonts';
 import routes from './routes';
+
+/**
+ * On client-side application initialization, inject global CSS styles into the document head.
+ * WARNING: The operations taken by this function are *stateful* and *have side effects*. This
+ * function should only be called once.
+ */
+function injectGlobalStyles() {
+  const fontsMeta = [
+    {
+      name: 'karla-regular',
+      data: fonts.karlaRegular,
+      file: 'karla-regular.ttf'
+    },
+    {
+      name: 'karla-bold',
+      data: fonts.karlaBold,
+      file: 'karla-bold.ttf'
+    },
+    {
+      name: 'source-code-pro-regular',
+      data: fonts.sourceCodeProRegular,
+      file: 'source-code-pro-regular.ttf'
+    },
+    {
+      name: 'source-code-pro-medium',
+      data: fonts.sourceCodeProMedium,
+      file: 'source-code-pro-medium.ttf'
+    }
+  ];
+
+  fontsMeta.forEach((font) => csjs.csjs([fonts.fontFaceStyle(font.name, font.data, font.file)]));
+}
+
+injectGlobalStyles();
 
 // Explicitly register supported languages for the syntax highlighter
 registerLanguage('curl', bash);
