5debe08bd5c7bed29dc8bb474ddea9b3f2009c21
Hemanth Devarapalli <itsmehemant123@gmail.com>
2018-01-25 05:03:40 -0500

    Handling password-less redis configs;

--- core/app.py
+++ core/app.py
@@ -31,11 +31,13 @@ def init_db():
 
 
 def init_cache():
+    redis_password = config.secrets.server('redis.password')
+    redis_password = None if redis_password == 'password' else redis_password
     return redis.StrictRedis(
         host=config.secrets.server('redis.host'),
         port=config.secrets.server('redis.port'),
         db=0,
-        password=config.secrets.server('redis.password'),
+        password=redis_password,
     )
 
 
